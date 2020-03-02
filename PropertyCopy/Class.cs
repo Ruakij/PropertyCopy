58c579309abc9ee5b5538ee5bfb11bf238261922
Railz <alexander.b@Railduction.eu>
2020-03-02 15:14:00 +0100

    Added Copy-Method

---
+++ PropertyCopy/Class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PropertyCopy
+{
+    public static class Class
+    {
+        public static void CopyPropertiesFrom(this object self, object parent)
+        {
+            var fromProperties = parent.GetType().GetProperties();
+            var toProperties = self.GetType().GetProperties();
+
+            foreach (var fromProperty in fromProperties)
+            {
+                foreach (var toProperty in toProperties)
+                {
+                    if (fromProperty.Name == toProperty.Name && fromProperty.PropertyType == toProperty.PropertyType)
+                    {
+                        toProperty.SetValue(self, fromProperty.GetValue(parent));
+                        break;
+                    }
+                }
+            }
+        }
+        
+    }
+}
--- PropertyCopy/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PropertyCopy
-{
-    public class Class1
-    {
-    }
-}
